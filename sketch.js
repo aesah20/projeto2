c54582442b79bd36e6beab1f46c643d7ac874acf
aesah20 <sara.reis.oliveira@escola.pr.gov.br>
2024-11-21 11:54:34 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let cor;
+let posicaoHorizontal = 200; 
+let posicaoVertical = 200;
+
+function setup() { 
+  createCanvas(400, 400);
+  background("rgb(187,11,11)");
+  cor= color(random(0,255)); random(0,255),random(0,255);
+}
+
+function draw() { 
+  circle(posicaoHorizontal,posicaoVertical, 50);
+  fill(cor);
+  if (mouseX > posicaoHorizontal){ 
+    posicaoHorizontal =
+      posicaoHorizontal +1;
+  }
+  if (mouseX < posicaoHorizontal){ 
+    posicaoHorizontal =
+      posicaoHorizontal -1;
+  }
+  if (mouseY > posicaoVertical){ 
+    posicaoVertical = posicaoVertical
+  +1;
+  }
+  if (mouseY < posicaoVertical){ 
+    posicaoVertical = posicaoVertical
+    -1;
+  }
+  if
+    (mouseIsPressed){ 
+      cor = color((random(0, 255)),
+                  random(0, 255), random(0, 255),
+                  random(0,100));
+    }
+}
+  
+  
+  
+  
